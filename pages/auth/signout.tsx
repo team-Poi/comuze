8b708b6e6e72994b4b12ea18889ab7d62b900d7f
Oein <oein0219@gmail.com>
2023-06-25 13:50:01 +0900

    signout redirect page

---
+++ pages/auth/signout.tsx
@@ -0,0 +1,11 @@
+import { signOut } from "next-auth/react";
+import { useEffect } from "react";
+
+export default function Page() {
+  useEffect(() => {
+    signOut({
+      callbackUrl: "/",
+    });
+  }, []);
+  return <div>Redirecting to Auth0...</div>;
+}
