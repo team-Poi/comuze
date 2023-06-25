5920f9a88c59d173c6db9c34e61f938de132d362
Oein <oein0219@gmail.com>
2023-06-25 13:50:32 +0900

    add providers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { ModalProvider } from "@team.poi/ui/dist/cjs/hooks/Modal";
+import "@team.poi/ui/src/styles/globals.css";
+import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <SessionProvider session={pageProps.session}>
+      <ModalProvider>
+        <Component {...pageProps} />
+      </ModalProvider>
+    </SessionProvider>
+  );
 }
