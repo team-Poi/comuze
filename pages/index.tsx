e87e2ae2fac06b8c6f088fa19932e709927f5d30
Oein <oein0219@gmail.com>
2023-06-25 16:02:55 +0900

    change main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
 import { Conatiner } from "@/components/Container";
 import Header from "@/components/Header";
+import { Icon } from "@/components/Icon";
 import styles from "@/styles/Home.module.css";
 export default function Home() {
   return (
     <>
       <Header type="MAIN" />
-      <Conatiner>
-        <div className={styles.uppermain}></div>
-        <div className={styles.lowermain}></div>
+      <Conatiner className={styles.main}>
+        <div className={styles.head}></div>
       </Conatiner>
     </>
   );
