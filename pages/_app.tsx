39994d2ba4e89df61e340247ab19c3831c6fff60
Oein <oein0219@gmail.com>
2023-06-27 07:51:27 +0900

    optionalSet check wrapper

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,36 @@ import "@/styles/globals.css";
 import { ModalProvider } from "@team.poi/ui/dist/cjs/hooks/Modal";
 import "@team.poi/ui/src/styles/globals.css";
 import type { AppProps } from "next/app";
-import { SessionProvider } from "next-auth/react";
+import { SessionProvider, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { ToastContainer } from "react-toastify";
 
-export default function App({ Component, pageProps }: AppProps) {
+function Wrapper({ Component, pageProps }: AppProps) {
+  let { status, data } = useSession();
+  let router = useRouter();
+  useEffect(() => {
+    if (status == "loading") return;
+    if (status == "unauthenticated") return;
+    if (status == "authenticated" && data?.user.age == undefined) {
+      router.push("/auth/optionalSet");
+      return;
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [status]);
+
   return (
-    <SessionProvider session={pageProps.session}>
-      <ModalProvider>
-        <Component {...pageProps} />
-      </ModalProvider>
+    <ModalProvider>
+      <Component {...pageProps} />
+      <ToastContainer autoClose={3000} />
+    </ModalProvider>
+  );
+}
+
+export default function App(props: AppProps) {
+  return (
+    <SessionProvider session={props.pageProps.session}>
+      <Wrapper {...props} />
     </SessionProvider>
   );
 }
