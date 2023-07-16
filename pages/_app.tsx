532301563ba08b65d1b18ae56207105f1bc10407
Oein <oein0219@gmail.com>
2023-07-16 22:46:24 +0900

    remove Gtag
    Co-authored-by: Sangho <sangho129@@icloud.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,9 +7,6 @@ import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { ToastContainer } from "react-toastify";
 
-import Script from "next/script";
-import * as gtag from "@/utils/gtag";
-
 import "react-toastify/dist/ReactToastify.css";
 import Head from "next/head";
 import { LoadingProvider } from "@/hooks/Loading";
@@ -38,39 +35,12 @@ function Wrapper({ Component, pageProps }: AppProps) {
 }
 
 export default function App(props: AppProps) {
-  const router = useRouter();
-  useEffect(() => {
-    const handleRouteChange = (url: string) => {
-      gtag.pageview(url);
-    };
-    router.events.on("routeChangeComplete", handleRouteChange);
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
   return (
     <>
       <Head>
         <meta property="og:title" content="Comuze / 학교 생활을 더 즐겁게" />
         <title>Comuze / 학교 생활을 더 즐겁게</title>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-
-              gtag('config', '${gtag.GA_TRACKING_ID}', {
-                page_path: window.location.pathname,
-              });
-            `,
-          }}
-        />
       </Head>
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-      />
       <SessionProvider session={props.pageProps.session}>
         <Wrapper {...props} />
       </SessionProvider>
