31e11a6905f34a23c078c5d981dd7d9ea8a99b0a
Oein <oein0219@gmail.com>
2023-07-17 17:45:01 +0900

    add pwa

--- next.config.js
+++ next.config.js
@@ -1,6 +1,27 @@
 /** @type {import('next').NextConfig} */
+
+const withPWA = require("next-pwa");
+const defaultRuntimeCaching = require("next-pwa/cache");
+
 const nextConfig = {
   reactStrictMode: false,
 };
 
-module.exports = nextConfig;
+module.exports = withPWA({
+  dest: "public",
+  runtimeCaching: [
+    {
+      urlPattern: "https://*/**/*.woff2",
+      method: "GET",
+      handler: "StaleWhileRevalidate",
+      options: {
+        cacheName: "fonts",
+        expiration: {
+          maxEntries: 64,
+          maxAgeSeconds: 24 * 60 * 60 * 30 * 365, // 1 Years
+        },
+      },
+    },
+    ...defaultRuntimeCaching,
+  ],
+})(nextConfig);
